60009e280a8e8b5fe4b833875ced12069582dc91
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-05-06 11:55:27 -0300

    add: jump_search realizado

---
+++ algoritmos/jump_search/jump_search.js
@@ -0,0 +1,24 @@
+//Tenho um arr de n elementos
+//Tenho um bloco a ser pulado de tamanho m
+
+const arr = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610];
+const len = arr.length;
+const skipBlock = 4;
+const target = 55;
+
+let indexResult = 0;
+let result = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i * skipBlock] < target && target < arr[(i + 1) * skipBlock]) {
+    const index = i * skipBlock;
+    for (let j = index; j < arr.length; j++) {
+      if (arr[j] == target) {
+        indexResult += j;
+        result += arr[j];
+      }
+    }
+  }
+}
+
+console.log("Number " + result + "is at index " + result);
