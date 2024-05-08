2ff5e59157c2337fc753e691a51634a75b718cf2
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-05-08 08:36:14 -0300

    add: minimum_p_sum realizado

---
+++ algoritmos/minimum_p_sum/minimum_p_sum.js
@@ -0,0 +1,20 @@
+//Encontre a soma dos produtos de dois arrays de mesmo tamanho, dado que k modificações foram permitidas no array 1
+//Em cada modificação, um elemento do array 1 pode ser tanto incrementado ou decerementado em 2
+
+const arr1 = [1, 2, -3];
+const arr2 = [-2, 3, -5];
+const k = 5;
+
+let indexElement = 2;
+let incDec = 2;
+let result = 0;
+
+for (let i = 0; i < k; i++) {
+  arr1[indexElement] += incDec;
+}
+
+for (let i = 0; i < arr1.length; i++) {
+  result += arr1[i] * arr2[i];
+}
+
+console.log(result);
