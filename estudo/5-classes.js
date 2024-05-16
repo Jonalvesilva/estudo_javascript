8eca465baab8bfc9a4019cdb8d92b2e8966e7519
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-05-16 10:15:18 -0300

    feat: add modulo 5 - classes

--- estudo/5-classes.js
+++ estudo/5-classes.js
@@ -51,4 +51,69 @@ class Car {
 //Para executarmos um metodo usamos assim:
 
 new Car("Ford", "2004").age(); // Neste exemplo eu não tinha instancia dele então tive que instancia lo para acessar seu metodo
-obj.age(); //Como aqui eu ja tinha instanciado e guardado na variavel objeto, basta apenas chamar o metodo pela variavel. Teste
+obj.age(); //Como aqui eu ja tinha instanciado e guardado na variavel objeto, basta apenas chamar o metodo pela variavel.
+
+//Uma classe tem o conceito de herança. Uma herança é quando temos uma classe que herda os atributos e metodos de outra classe
+
+//Estrutura Heranca
+
+class Car {
+  constructor(brand) {
+    this.carname = brand;
+  }
+  present() {
+    return "I have a " + this.carname;
+  }
+}
+
+class Model extends Car {
+  constructor(brand, mod) {
+    super(brand);
+    this.model = mod;
+  }
+  show() {
+    return this.present() + ", it is a " + this.model;
+  }
+}
+
+//Na primeira temos uma class car e na segunda temos uma classe model que herda a classe car
+
+//Assim conseguimos utilizar os atributos e metodos de uma classe em outra classe
+
+//Metodos getters and setters
+
+//Metodo getters: São metodos que apenas retornam algum atributo
+//Metodo setters: São metodos que alteram os atributos
+
+class Car {
+  constructor(brand) {
+    this.carname = brand;
+  }
+  get cnam() {
+    return this.carname;
+  }
+  set cnam(x) {
+    this.carname = x;
+  }
+}
+
+//Temos aqui uma classe car, onde temos um método get que retorna this.carname e um método set que recebe um parametro x e altera this.carname
+
+//As classes não fazem hoisting, ou seja, o javascript não joga as classes para o inicio do escopo
+
+//Assim, não é permitido instanciar um objeto antes da declaração da classe.
+
+//As classes tem o conceito de interface, mas será visto no Typescript
+
+//Os metodos e atributos podem ser static, ou seja, podem ser usadas pela propria classe, sem a necessidade de instanciar um objeto
+
+class Car {
+  constructor(name) {
+    this.name = name;
+  }
+  static hello() {
+    return "Hello!!";
+  }
+}
+
+Car.hello(); //Note que aqui eu não precisei instanciar um objeto usando o new Car para acessar o metodo hello
