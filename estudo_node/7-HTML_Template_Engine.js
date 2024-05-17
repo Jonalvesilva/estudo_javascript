c25d4fcb18eebb491af39248d49bc021a76d9ca0
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-05-17 09:18:12 -0300

    feat: add modulo 7 - Html Template Engine

---
+++ estudo_node/7-HTML_Template_Engine.js
@@ -0,0 +1,15 @@
+//Node tem vários engines que criam paginas ou snippets para saida
+
+//Um tipico engine pegará um template HTML e:
+
+//-Substiutui variaveis por atuais valores
+//-Permite inclusão de headers, footers, etc
+//-Permite o basico da programação como condiçoes e loops
+
+//Com ele é possivel pré-renderizar algumas partes do template
+
+//Um engine mais famoso é o EJS
+
+//Para começar a usar o EJS: npm install --save ejs
+
+//As pre-renders geralmente ficam em um subdiretorio do projeto chamado view.
