4e1abe0c3786f22078ea275dc7cc6be017eda76b
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-06-02 11:45:34 -0300

    feat: finished modulo 1 - intro

---
+++ estudo_react/1-intro.ts
@@ -0,0 +1,13 @@
+//React
+
+//React é um framework Javascript criado pelo Facebook que é usado para criar interfaces de usuário em aplicativos web.
+
+//Ele é popular por ser facil de usar, altamente flexivel e escalavel, e é usado por muitas empresas de tecnologia, incluindo o Facebook, Instagram, etc.
+
+//Em resumo, o mercado de trabalho para desenvolvedores, o React é muito bom, com muitas tecnologia que usam esse framework em suas aplicações.
+
+//Ferramentas React
+
+//Existem varias ferramentas que trabalham com o React
+
+//Vite, Create-react-app e Next
