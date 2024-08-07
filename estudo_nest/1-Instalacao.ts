0ee3e14749f0ae644a623124a87498b106216c02
Jonathan Silva <jonalves.silva@jsmail.com.br>
2024-08-07 08:15:16 -0300

    feat: add 1-Instalacao

---
+++ estudo_nest/1-Instalacao.ts
@@ -0,0 +1,12 @@
+/*
+
+Para instalar o Nest e criar uma nova aplicação:
+     npm i -g @nestjs/cli
+     npx new project-name
+
+Depois instalar seus tipos:
+    npm i --save @nestjs/core @nestjs/common rxjs reflect-metadata
+
+Depois inicie:
+    npm run start
+*/
